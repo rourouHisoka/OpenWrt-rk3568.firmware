ab746f59539148087717a4cdb65028b12cd87a1d
rourouHisoka <111048008+rourouHisoka@users.noreply.github.com>
2022-08-14 10:51:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,13 +25,4 @@ echo 'src-git luci-app-passwall https://github.com/xiaorouji/openwrt-passwall/tr
 echo 'src-git filebrowser https://github.com/kenzok8/openwrt-packages/tree/master/filebrowser' >>feeds.conf.default
 echo 'src-git luci-app-filebrowser https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-filebrowser' >>feeds.conf.default
 
-# Mod zzz-default-settings
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-popd
-
 
